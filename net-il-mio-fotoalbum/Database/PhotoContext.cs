a2332d7113e42398b68fee66136426615b1f1020
David Dume <dumedavid11@gmail.com>
2023-10-11 15:00:09 +0200

    aggiunta modelli e collegamento db

---
+++ net-il-mio-fotoalbum/Database/PhotoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Database
+{
+    public class PhotoContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public PhotoContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=Photo;Integrated Security=True;TrustServerCertificate=true;");
+        }
+    }
+}
---
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -0,0 +1,27 @@
+﻿namespace net_il_mio_fotoalbum.Models
+{
+    public class Photo
+    {
+        public Photo() { }
+
+        public Photo(string title, string description, string url, bool visible)
+        {
+            Title = title;
+            Description = description;
+            Url = url;
+            Visible = visible;
+        }
+
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+
+        public bool Visible { get; set; }
+
+        public List<Category>? Categories { get; set; }
+    }
+}
