8f91501527fe86a179538a030b716356717fb53d
David Dume <dumedavid11@gmail.com>
2023-10-12 15:52:04 +0200

    aggiunta messaggi categorie e varie viste

--- net-il-mio-fotoalbum/Database/PhotoContext.cs
+++ net-il-mio-fotoalbum/Database/PhotoContext.cs
@@ -9,6 +9,7 @@ namespace net_il_mio_fotoalbum.Database
     {
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         public PhotoContext() { }
 
